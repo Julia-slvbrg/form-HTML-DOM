2bd1a68c2c93730187741a2850b79af3cee5f400
Julia-slvbrg <julia.slvb@gmail.com>
2024-01-15 19:54:41 -0300

    fix: comparison bug fixed

--- script.js
+++ script.js
@@ -64,18 +64,28 @@ form.addEventListener('submit', function (e){
     const inputA = document.getElementById('inputA');
     const inputB = document.getElementById('inputB');
 
+    const inputAToNum = Number(inputA.value);
+    const inputBToNum = Number(inputB.value);
+
     const invalidMesssageParagraph = document.getElementById('invalid-message');
     const validMessageParagraph = document.getElementById('valid-message');
     validMessageParagraph.style.display = 'none';
     invalidMesssageParagraph.style.display = 'none';
 
-    if(inputB.value > inputA.value){
+    if(inputAToNum < inputBToNum){
         const validMessage = `<b>VÁLIDO!!</b> O número ${inputB.value} é maior que o número ${inputA.value}`;
         validMessageParagraph.innerHTML = validMessage;
         validMessageParagraph.style.display = 'flex'
+
+        inputA.value = '';
+        inputB.value = '';
+        
     }else{
         const invalidMesssage = `<b>INVÁLIDO!!</b> O número ${inputB.value} não é maior que o número ${inputA.value}`;
-       invalidMesssageParagraph.innerHTML = invalidMesssage;
+        invalidMesssageParagraph.innerHTML = invalidMesssage;
         invalidMesssageParagraph.style.display = 'flex'
+
+        inputA.value = '';
+        inputB.value = '';
     }
 })
